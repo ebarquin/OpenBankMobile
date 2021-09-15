2b086bc441993213511e86b5ee6a166f2ee3485f
Eugenio Barquin <eu.barquin@me.com>
2021-09-15 17:55:22 +0200

    empty project

---
+++ OpenBankMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenBankMobile
+//
+//  Created by Eugenio Barquín on 15/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
